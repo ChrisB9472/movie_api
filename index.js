f7b509ff0a41ea443a59f7d76f8e6a6a2c5bf2f7
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2021-11-30 15:37:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const Genre = Models.Genre;
 const Users = Models.User;
 require('dotenv').config();
 
-//mongoose.connect('mongodb://localhost:27017/MovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/MovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
