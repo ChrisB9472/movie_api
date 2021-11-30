ee62b7e4ead0a72dbaa8f61295906bdd6f535f7b
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2021-11-30 14:08:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const Movies = Models.Movie;
 const Genre = Models.Genre;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/MovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/MovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
