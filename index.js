ba11aabaab1aeeb4a7e02bf33944b53d08169225
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2021-12-02 14:08:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 //mongoose.connect('mongodb://localhost:27017/MovieDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect( 'process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( 'mongodb+srv://myFlixDB:myFlixDB@myflixdb.d5rmr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
