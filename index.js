9a43ac86840816fbeb47ac2b60cc0541ea18b2e1
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-18 12:01:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,7 @@ mongoose.connect('mongodb+srv://myFlixDB:myFlixDB@myflixdb.d5rmr.mongodb.net/myF
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://chris-movies.netlify.app/ '];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
